89cf5d6b16d86ea829d9246b801b7daecba0ea1e
Jxrom <68335788+Jxrom@users.noreply.github.com>
2023-05-27 22:31:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def main():
     st.write("# 🧪Urinity App")
     st.write("### Benedict’s Reagent Classifier")
     st.write("The Benedict's Reagent Classifier is a deep learning model that classifies images of Benedict's solution test tubes into different glucose concentration levels.")
-    st.write("The Benedict's Reagent Classifier utilizes transfer learning techniques, specifically ResNet, MobileNet, InceptionV3, and VGG16, to automate and simplify the process of determining glucose levels. By analyzing images of Benedict's solution test tubes, the classifier predicts the concentration of glucose, offering a rapid and efficient method for analysis.")
+    st.write("The Benedict's Reagent Classifier utilizes transfer learning techniques, specifically MobileNet, ResNet, InceptionV3, and VGG16, to automate and simplify the process of determining glucose levels. Among these models, MobileNet demonstrates exceptional accuracy in analyzing images of Benedict's solution test tubes and predicting the concentration of glucose, offering a rapid and efficient method for analysis.")
 
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Go to", ("Home", "About Us"))
