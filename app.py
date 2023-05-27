7c8fdb2a077e5f205d1c2abd5a36f20c7c2fc609
Jxrom <68335788+Jxrom@users.noreply.github.com>
2023-05-27 20:21:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,27 +5,39 @@ import numpy as np
 import cv2
 
 def main():
-    st.title("Streamlit App")
-   
-    @st.cache_resource
-    def load_model():
-        model = tf.keras.models.load_model('test_tube_classifier.hdf5')
+    st.title("Urinalysis")
+
+    @st.cache(allow_output_mutation=True)
+    def load_model(model_name):
+        if model_name == "VGG16":
+            model = tf.keras.models.load_model('vgg16_urinalysis.hdf5')
+        elif model_name == "ResNet50":
+            model = model = tf.keras.models.load_model('resnet50_urinalysis.hdf5')
+        elif model_name == "InceptionV3":
+            model = model = tf.keras.models.load_model('inceptionv3_urinalysis.hdf5')
+        elif model_name == "MobileNet":
+            model = model = tf.keras.models.load_model('mobilenet_urinalysis.hdf5')
+        else:
+            raise ValueError("Invalid model name")
         return model
-    
+
     def import_and_predict(image_data, model):
-        size=(128,128)
-        image = ImageOps.fit(image_data,size, Image.LANCZOS)
+        size=(160, 160)
+        image = ImageOps.fit(image_data, size, Image.LANCZOS)
         image = np.asarray(image)
         image = image / 255.0
-        img_reshape = np.reshape(image, (1, 128, 128, 3))
+        img_reshape = np.reshape(image, (1, 160, 160, 3))
         prediction = model.predict(img_reshape)
         return prediction
 
-    model = load_model()
-    class_names =  ["High", "Moderate", "No reducing sugar", "Traceable"]
+    model_names = ["VGG16", "ResNet50", "InceptionV3", "MobileNet"]
+    selected_model = st.selectbox("Select a model", model_names)
+
+    model = load_model(selected_model)
+    class_names = ["High", "Moderate", "No reducing sugar", "Traceable"]
 
     st.write("# Glucose Level (Benedict Solution) Classifier")
-   
+
     file = st.file_uploader("Choose photo from computer", type=["jpg", "png", "jpeg"])
 
     if file is None:
@@ -38,6 +50,6 @@ def main():
         class_name = class_names[class_index]
         string = "Glucose Level: " + class_name
         st.success(string)
- 
+
 if __name__ == "__main__":
     main()
